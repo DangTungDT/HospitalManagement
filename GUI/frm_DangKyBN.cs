fc272c072f3738ecb5d10acb496a4bc27fe8103a
vyva2004 <vyva2004@gmail.com>
2025-06-13 10:24:25 +0700

    thiet ke form

---
+++ GUI/frm_DangKyBN.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frm_DangKyBN : Form
+    {
+        public frm_DangKyBN()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
