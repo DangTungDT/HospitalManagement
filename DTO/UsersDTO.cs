7218272c58820d3db287264b8d0363970fefb122
Hung04102005 <trongtin1122nak@gmail.com>
2025-06-20 20:06:37 +0700

    add form Dang Ky

---
+++ DTO/UsersDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class UsersDTO
+    {
+        public int UserID { get; set; } // Khóa chính
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public string StaffID { get; set; }
+        public string Status { get; set; }
+    }
+}
