6af7f33cf1429257e7a8cf8f78f388d5953c1624
Dang Thanh Tung <thanhtungdangdtt@gmail.com>
2025-06-21 09:08:21 +0700

    Cập nhật thêm form quản lý kho và thuốc

--- Database/Database.sql
+++ Database/Database.sql
@@ -8,137 +8,243 @@ GO
 set dateformat dmy
 go
 
----- Table User (Tài khoản)
--- Giải thích : Lưu thông tin tài khoản (admin, bác sĩ, bệnh nhân, v.v.), với mật khẩu được mã hóa (PasswordHash)
-
-
-
-CREATE TABLE Users (
-    UserID INT PRIMARY KEY IDENTITY(1,1),
-    Username VARCHAR(50) NOT NULL UNIQUE,
-    PasswordHash VARCHAR(255) NOT NULL,
-    FullName NVARCHAR(100) NOT NULL,
-    Email VARCHAR(100) UNIQUE,
-    Role VARCHAR(20) NOT NULL CHECK (Role IN ('Admin', 'Doctor', 'Nurse', 'Receptionist')),
-    CreatedAt DATETIME DEFAULT GETDATE(),
-	Address NVARCHAR(255),
-    IsActive BIT DEFAULT 1 --Xác định tài khoản có đang hoạt động hay bị vô hiệu hóa (1 là hoạt động, 0 là vô hiệu hóa)
+--Table Staff
+--Bảng nhân viên
+CREATE TABLE Staff (
+    id CHAR(10) PRIMARY KEY,
+    name NVARCHAR(255) NOT NULL,
+    role NVARCHAR(50),
+    dob DATE,
+    gender NVARCHAR(10) CHECK (gender IN ('Male', 'Female', 'Other')),
+    phoneNumber VARCHAR(15),
+    email VARCHAR(100),
+    homeAddress VARCHAR(255),
+    citizenID VARCHAR(12),
+    departmentID char(10),
+    position VARCHAR(50),
+    qualification VARCHAR(100),
+    degree VARCHAR(100),
+    salaryID INT,
+    status NVARCHAR(20) DEFAULT 'Active' CHECK (status IN ('Active', 'Inactive', 'On Leave')),
+	startDate date,
+    Notes text
 )
 go
 
----- Table for Department (Khoa)
---Giải thích: Quản lý các khoa trong bệnh viện.
-CREATE TABLE Department (
-    DepartmentID INT PRIMARY KEY IDENTITY(1,1),
-    DepartmentName NVARCHAR(100) NOT NULL UNIQUE,
-    Description NVARCHAR(255)
+--Table Account
+create table Account
+(
+	id int primary key identity(1,1),
+	username char(50),
+	password char(255),
+	startDate date,
+	staffID char(10),
+	status char(100),
 )
 go
 
----- Table for Doctor (Bác sĩ)
--- Giải thích: Lưu thông tin bác sĩ, liên kết với Users (Tài khoản) và Department (Khoa)
-CREATE TABLE Doctor (
-    DoctorID char(10) PRIMARY KEY,
-    UserID INT NOT NULL,
-    DepartmentID INT NOT NULL,
-    Specialty NVARCHAR(100),
-    Phone VARCHAR(15),
-	Address NVARCHAR(255), 
-    FOREIGN KEY (UserID) REFERENCES Users(UserID),
-    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+--Table Room
+create table Room
+(
+	id char(10) primary key,
+	roomName nvarchar(100),
+	bedCount int,
+	departmentID char(10)
 )
 go
 
--- Table for Doctor Qualifications (Bằng cấp bác sĩ)
--- Giải thích: Lưu các bằng cấp cho Doctor (Vì 1 doctor bằng buộc phải ít nhất 1 bằng cấp và có thể có nhiều)
-CREATE TABLE DoctorQualifications (
-    QualificationID INT PRIMARY KEY IDENTITY(1,1),
-    DoctorID char(10) NOT NULL,
-    QualificationName NVARCHAR(100) NOT NULL,
-    IssuingOrganization NVARCHAR(100),
-    IssueDate DATE,
-    FOREIGN KEY (DoctorID) REFERENCES Doctor(DoctorID)
+--Table Appointment
+create table Appointment
+(
+	id int primary key identity(1,1),
+	startDate dateTime,
+	note text,
+	status nvarchar(100),
+	doctorID char(10),
+	patientID char(10)
+)
+go
+--Table DailyCare
+create table DailyCare
+(
+	id int primary key identity(1,1),
+	shift nvarchar(100),
+	bloodPressure varchar(20),
+	bodyTempearature DECIMAL(4,1), --Unit độ C
+	pulseRate int,
+	note text,
+	patientID char(10),
+	roomID char(10),
+	nurseID char(10)
 )
 go
 
--- Table for Patient (Bệnh nhân)
--- Giải thích: Lưu thông tin bệnh nhân, liên kết với Users.
+--Table MedicalOrder
+CREATE TABLE MedicalOrder (
+    id INT PRIMARY KEY identity(1,1),     -- Mã y lệnh
+    PatientID CHAR(10) NOT NULL,               -- FK → Patient
+    DoctorID CHAR(10) NOT NULL,                -- FK → Doctor
+    OrderType VARCHAR(50) NOT NULL,            -- Loại y lệnh: Thuốc / Xét nghiệm / Chỉ định khác
+    ItemID char(10) NULL,                      -- FK → Medicine hoặc LabTestType
+    TestTypeID INT NULL,                       -- FK → LabTestType (nếu là xét nghiệm)
+    Dosage VARCHAR(100) NULL,                  -- Liều lượng (Dosage)
+    Quantity DECIMAL(8,2) NULL,                -- Số lượng (Quantity)
+    Unit VARCHAR(20) NULL,                     -- Đơn vị (Unit)
+    Frequency VARCHAR(50) NULL,                -- Tần suất (ví dụ: 2 lần/ngày)
+    StartDate DATE NULL,                       -- Ngày bắt đầu y lệnh
+    EndDate DATE NULL,                         -- Ngày kết thúc (nếu có)
+    Status VARCHAR(20) DEFAULT 'Active',       -- Trạng thái: Active / Completed / Discontinued
+    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Thời gian ghi y lệnh
+    SignedAt DATETIME NULL,                    -- Thời gian bác sĩ ký duyệt
+    Note TEXT NULL                            -- Ghi chú thêm
+    -- có thể thêm FK tới các bảng Medicine hoặc LabTestType nếu lưu chi tiết
+)
+go
+
+--table Patient
 CREATE TABLE Patient (
-    PatientID char(10) PRIMARY KEY,
-    DateOfBirth DATE,
-    Gender VARCHAR(10) CHECK (Gender IN ('Male', 'Female', 'Other')),
-    Address NVARCHAR(255),
-	Weight float check (Weight >0),
-	Height float check (Height >0),
-    Phone VARCHAR(15),
-	Role nvarchar(25) check (Role in ('Outpatient', 'Inpatient')),
-	DoctorID char(10) not null,
-	Foreign key (DoctorID) REFERENCES Doctor(DoctorID)
+    id CHAR(10) PRIMARY KEY,
+    fullName NVARCHAR(255) NOT NULL,
+    gender NVARCHAR(10),                -- Giới tính: Nam/Nữ/Khác
+    dob DATE,                           -- Ngày sinh
+    phoneNumber VARCHAR(15),
+    citizenID VARCHAR(20),             -- CCCD/CMND
+    InsuranceID VARCHAR(20),           -- Mã bảo hiểm y tế
+    address NVARCHAR(255),
+    EmergencyContact NVARCHAR(255),    -- Tên người liên hệ khẩn cấp
+    EmergencyPhone VARCHAR(15),
+    status NVARCHAR(50),               -- Trạng thái (đang điều trị, xuất viện,...)
+    createdDate DATETIME DEFAULT GETDATE(),
+    updatedDate DATETIME DEFAULT GETDATE(),
+    weight FLOAT,                      -- Cân nặng (kg)
+    height FLOAT                       -- Chiều cao (cm)
 )
 go
 
-----Table for Nurse
--- Giải thích: Quản lý thông tin nhân viên điều dưỡng
-CREATE TABLE Nurse
+--Table Doctor_Patient
+create table DoctorPatient
 (
-	NurseID char(10) PRIMARY KEY,
-    UserID INT NOT NULL,
-    DepartmentID INT NOT NULL,
-    Phone VARCHAR(15),
-	Address NVARCHAR(255), 
-    FOREIGN KEY (UserID) REFERENCES Users(UserID),
-    FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+	doctorID char(10),
+	patientID char(10),
+	startDate date,
+	endDate date,
+	role varchar(50), --vai trò bác sĩ: khám chính, hội chẩn, điều trị, khám ngoại trú, khám nội trú,...
+	note text,
+	primary key(doctorID, patientID, startDate) 
 )
---Table for Room
----- Giải thích: bảng quản lý thông tin phòng
-CREATE TABLE ROOM
+go
+
+--Table Department
+create table Department
 (
-	RoomID char(10) primary key,
-	RoomName nvarchar(100) UNIQUE,
-	NumberBed int default(0),
+	id char(10),
+	departmentName nvarchar(255),
+	description text,
+	primary key(id)
+)
+go
+
+--Table MedicalRecord
+CREATE TABLE MedicalRecord (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    patientID CHAR(10) NOT NULL,
+    doctorID CHAR(10) NOT NULL,
+    diagnosis NVARCHAR(500),
+    treatmentPlan NVARCHAR(500),
+    prescription NVARCHAR(500),
+    vitalSigns NVARCHAR(255),
+    createdDate DATETIME DEFAULT GETDATE(),
+    notes NVARCHAR(1000),
+)
+go
+
+--Table laboratoryTest
+CREATE TABLE LaboratoryTest (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    testName NVARCHAR(255) NOT NULL,
+    patientID CHAR(10) NOT NULL,
+    doctorID CHAR(10) NOT NULL,
+    startDate DATETIME,
+    resultValue NVARCHAR(100),
+    resultUnit NVARCHAR(50),
+    result NVARCHAR(255),
+    testType NVARCHAR(100),
+    status NVARCHAR(50),
+    note text,
+)
+go
+
+--Table SupplyHistory 
+CREATE TABLE SupplyHistory (
+    id CHAR(10) PRIMARY KEY,
+    itemID CHAR(10) NOT NULL,
+    roomID CHAR(10) NOT NULL,
+    nurseID CHAR(10) NOT NULL,
+    dosage VARCHAR(255),
+    quantity INT,
+    unit VARCHAR(255),
+    note text
+)
+go
+
+--Table Items
+CREATE TABLE Items (
+    id CHAR(10) PRIMARY KEY,        -- Mã vật tư (duy nhất)
+    itemName NVARCHAR(255) NOT NULL, -- Tên vật tư (dùng NVARCHAR để hỗ trợ tiếng Việt)
+    itemType NVARCHAR(100),          -- Loại vật tư (ví dụ: thuốc, thiết bị, dụng cụ...)
+	unit nvarchar(20),
+	price DECIMAL(18, 2)
 
 )
 go
 
---Table for Shift
----- Giải thích : bảng quản lý thông tin phân công điều dưỡng
-Create table ShiftNurse
-(
-	ShiftNurse int primary key identity(1,1),
-	ShiftType VARCHAR(50) check (ShiftType in ('Morning', 'Afternoon', 'Evening')),
-	NurseID char(10) not null,
-	ShiftDate date not null,
-	StartTime time not null,
-	EndTime time not null,
-	Notes nvarchar(255),
-	FOREIGN KEY (NurseID) REFERENCES Nurse(NurseID)
-)
-
-create table Room_ShiftNurse
-(
-	RoomID char(10),
-	ShiftNurse int,
-	primary key(RoomID, ShiftNurse),
-	FOREIGN KEY (RoomID) REFERENCES Room(RoomID),
-	FOREIGN KEY (ShiftNurse) REFERENCES ShiftNurse(ShiftNurse)
-)
-
----- Table for Appointment (Lịch hẹn)
--- Giải thích: Quản lý lịch hẹn, liên kết với Patient và Doctor.
-CREATE TABLE Appointment 
-(
-    AppointmentID INT PRIMARY KEY IDENTITY(1,1),
-    PatientID CHAR(10) NOT NULL,
-    DoctorID CHAR(10) NOT NULL,
-    AppointmentDate DATETIME NOT NULL,
-    Status VARCHAR(20) CHECK (Status IN ('Scheduled', 'Completed', 'Cancelled')),
-    Notes NVARCHAR(500),
-    CreatedAt DATETIME DEFAULT GETDATE(),
-    FOREIGN KEY (PatientID) REFERENCES Patient(PatientID),
-    FOREIGN KEY (DoctorID) REFERENCES Doctor(DoctorID)
-)
+--Foreign key
+alter table Account
+add constraint fk_Staff_Account foreign key(staffID) references Staff(id);
 go
 
+alter table Appointment
+add constraint fk_Doctor_Appointment foreign key(doctorID) references Staff(id),
+	constraint fk_patient_Appointment foreign key(patientID) references Patient(id);
+go
+
+alter table DailyCare
+add constraint fk_Patient_DailyCare foreign key(patientID) references Patient(id),
+	constraint fk_Room_DailyCare foreign key(roomID) references Room(id),
+	constraint fk_Nurse_DailyCare foreign key(nurseID) references Staff(id);
+go
+
+alter table MedicalOrder
+add constraint fk_Patient_MedicalOrder foreign key(PatientID) references Patient(id),
+	constraint fk_Doctor_MedicalOrder foreign key(DoctorID) references Staff(id),
+	constraint fk_Items_MedicalOrder foreign key(ItemID) references Items(id),
+	constraint fk_TestType_MedicalOrder foreign key(TestTypeID) references LaboratoryTest(id);
+go
+
+alter table DoctorPatient
+add constraint fk_Doctor_DP foreign key(doctorID) references Staff(id),
+	constraint fk_Patient_DP foreign key(patientID) references Patient(id);
+go
+
+alter table Staff
+add constraint fk_Department_Staff foreign key(departmentID) references Department(id);
+go
+
+alter table MedicalRecord
+add constraint fk_Doctor_MedicalRecord foreign key(doctorID) references Staff(id),
+	constraint fk_Patient_MedicalRecord foreign key(patientID) references Patient(id);
+go
+
+alter table LaboratoryTest
+add constraint fk_Doctor_LaboratoryTest foreign key(doctorID) references Staff(id),
+	constraint fk_Patient_LaboratoryTest foreign key(patientID) references Patient(id);
+go
+
+alter table SupplyHistory
+add constraint fk_Item_SupplyHistory foreign key(itemID) references Items(id),
+	constraint fk_Room_SupplyHistory foreign key(roomID) references Room(id),
+	constraint fk_Nurse_SupplyHistory foreign key(nurseID) references Staff(id);
+go
 
 --USE master;
 --ALTER DATABASE HospitalManagement SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
---
+++ GUI/FormHistoryOutItem.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace GUI
+{
+    partial class FormHistoryOutItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnViewDetail = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgvHistoryItemOut = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtItemName = new System.Windows.Forms.TextBox();
+            this.txtItemID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRecipientName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDispatcherName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dtpDateOutItem = new System.Windows.Forms.DateTimePicker();
+            this.panel5.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistoryItemOut)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel5.Controls.Add(this.label4);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(222, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(755, 70);
+            this.panel5.TabIndex = 5;
+            // 
+            // btnViewDetail
+            // 
+            this.btnViewDetail.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnViewDetail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnViewDetail.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnViewDetail.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewDetail.Location = new System.Drawing.Point(553, 18);
+            this.btnViewDetail.Margin = new System.Windows.Forms.Padding(0);
+            this.btnViewDetail.Name = "btnViewDetail";
+            this.btnViewDetail.Size = new System.Drawing.Size(193, 60);
+            this.btnViewDetail.TabIndex = 3;
+            this.btnViewDetail.Text = "Xem chi tiết";
+            this.btnViewDetail.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(330, 70);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Lịch sử xuất hàng";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dtpDateOutItem);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.txtDispatcherName);
+            this.panel2.Controls.Add(this.txtRecipientName);
+            this.panel2.Controls.Add(this.btnFind);
+            this.panel2.Controls.Add(this.txtItemName);
+            this.panel2.Controls.Add(this.txtItemID);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(222, 653);
+            this.panel2.TabIndex = 4;
+            // 
+            // dgvHistoryItemOut
+            // 
+            this.dgvHistoryItemOut.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHistoryItemOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvHistoryItemOut.Location = new System.Drawing.Point(0, 0);
+            this.dgvHistoryItemOut.Margin = new System.Windows.Forms.Padding(20);
+            this.dgvHistoryItemOut.Name = "dgvHistoryItemOut";
+            this.dgvHistoryItemOut.RowHeadersWidth = 51;
+            this.dgvHistoryItemOut.RowTemplate.Height = 24;
+            this.dgvHistoryItemOut.Size = new System.Drawing.Size(977, 653);
+            this.dgvHistoryItemOut.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.btnViewDetail);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(222, 553);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(755, 100);
+            this.panel1.TabIndex = 7;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnFind.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnFind.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFind.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.Location = new System.Drawing.Point(11, 571);
+            this.btnFind.Margin = new System.Windows.Forms.Padding(0);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(200, 70);
+            this.btnFind.TabIndex = 6;
+            this.btnFind.Text = "Tìm";
+            this.btnFind.UseVisualStyleBackColor = false;
+            // 
+            // txtItemName
+            // 
+            this.txtItemName.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtItemName.Location = new System.Drawing.Point(9, 182);
+            this.txtItemName.Multiline = true;
+            this.txtItemName.Name = "txtItemName";
+            this.txtItemName.Size = new System.Drawing.Size(202, 30);
+            this.txtItemName.TabIndex = 4;
+            this.txtItemName.Enter += new System.EventHandler(this.txtItemName_Enter);
+            this.txtItemName.Leave += new System.EventHandler(this.txtItemName_Leave);
+            // 
+            // txtItemID
+            // 
+            this.txtItemID.Location = new System.Drawing.Point(9, 110);
+            this.txtItemID.Multiline = true;
+            this.txtItemID.Name = "txtItemID";
+            this.txtItemID.Size = new System.Drawing.Size(202, 30);
+            this.txtItemID.TabIndex = 5;
+            this.txtItemID.Enter += new System.EventHandler(this.txtItemID_Enter);
+            this.txtItemID.Leave += new System.EventHandler(this.txtItemID_Leave);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Lime;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 70);
+            this.label2.TabIndex = 3;
+            this.label2.Tag = "";
+            this.label2.Text = "Tìm Kiếm";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtRecipientName
+            // 
+            this.txtRecipientName.Location = new System.Drawing.Point(9, 253);
+            this.txtRecipientName.Multiline = true;
+            this.txtRecipientName.Name = "txtRecipientName";
+            this.txtRecipientName.Size = new System.Drawing.Size(202, 30);
+            this.txtRecipientName.TabIndex = 8;
+            this.txtRecipientName.Enter += new System.EventHandler(this.txtRecipientName_Enter);
+            this.txtRecipientName.Leave += new System.EventHandler(this.txtRecipientName_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Mã hàng hóa";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Tên hàng hóa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Tên người nhận";
+            // 
+            // txtDispatcherName
+            // 
+            this.txtDispatcherName.Location = new System.Drawing.Point(9, 330);
+            this.txtDispatcherName.Multiline = true;
+            this.txtDispatcherName.Name = "txtDispatcherName";
+            this.txtDispatcherName.Size = new System.Drawing.Size(202, 30);
+            this.txtDispatcherName.TabIndex = 8;
+            this.txtDispatcherName.Enter += new System.EventHandler(this.txtDispatcherName_Enter);
+            this.txtDispatcherName.Leave += new System.EventHandler(this.txtDispatcherName_Leave);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 311);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Tên người xuất";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 394);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Ngày xuất";
+            // 
+            // dtpDateOutItem
+            // 
+            this.dtpDateOutItem.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDateOutItem.Location = new System.Drawing.Point(9, 413);
+            this.dtpDateOutItem.Name = "dtpDateOutItem";
+            this.dtpDateOutItem.Size = new System.Drawing.Size(200, 22);
+            this.dtpDateOutItem.TabIndex = 12;
+            // 
+            // FormHistoryItemOut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 653);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.dgvHistoryItemOut);
+            this.Name = "FormHistoryItemOut";
+            this.Text = "Lịch sử xuất hàng";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormHistoryItemOut_Load);
+            this.panel5.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistoryItemOut)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnViewDetail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvHistoryItemOut;
+        private System.Windows.Forms.TextBox txtRecipientName;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtItemName;
+        private System.Windows.Forms.TextBox txtItemID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDispatcherName;
+        private System.Windows.Forms.DateTimePicker dtpDateOutItem;
+    }
+}
