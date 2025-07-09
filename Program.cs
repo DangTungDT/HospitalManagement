1382480f7dd7ddd6de3f120363736a235af55078
Hung04102005 <trongtin1122nak@gmail.com>
2025-07-09 21:43:29 +0700

    Remove duplicated Program.cs and extra HospitalManagement folder

--- Program.cs
+++
@@ -1,9 +0,0 @@
-using GUI;
-
-static void Main()
-{
-    Application.EnableVisualStyles();
-    Application.SetCompatibleTextRenderingDefault(false);
-    // Chạy thử form thông tin nhân viên với username mẫu
-    Application.Run(new frmUserInformation("admin"));
-} 
